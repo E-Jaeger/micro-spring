eebb3f3641a35a68c36a6286c405117ebb3b8626
E-Jaeger <eren-jaeger@foxmail.com>
2021-11-05 13:37:19 +0800

    add step-01

--- micro-spring-step-01/src/main/java/org/ejaeger/springframework/BeanFactory.java
+++ micro-spring-step-01/src/main/java/org/ejaeger/springframework/BeanFactory.java
@@ -1,13 +1,8 @@
-package cn.bugstack.springframework;
+package org.ejaeger.springframework;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- */
 public class BeanFactory {
 
     private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap();
--- micro-spring-step-01/src/test/java/org/ejaeger/springframework/ApiTest.java
+++ micro-spring-step-01/src/test/java/org/ejaeger/springframework/ApiTest.java
@@ -1,2 +1,22 @@
-package org.ejaeger.springframework;public class ApiTest {
+package org.ejaeger.springframework;
+
+import org.ejaeger.springframework.bean.UserService;
+
+public class ApiTest {
+    public static void main(String[] args) {
+        test_BeanFactory();
+    }
+
+    public static void test_BeanFactory(){
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注入bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
 }
